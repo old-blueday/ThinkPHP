30c3eec254f72c302c88d53aae356a9426ab27f7
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-02-22 06:07:25 +0000

    git-svn-id: http://thinkphp.googlecode.com/svn/trunk@2775 539fd3ec-2725-0410-b7ab-251e373a8e33

--- Examples/Blog/index.php
+++ Examples/Blog/index.php
@@ -3,5 +3,4 @@
 define('APP_NAME', 'Blog');
 define('APP_PATH', './');
 // 加载框架入口文件
-define('APP_DEBUG',1);
 require( "../../ThinkPHP/ThinkPHP.php");
--- Examples/Tag/index.php
+++ Examples/Tag/index.php
@@ -2,6 +2,5 @@
 //定义项目名称和路径
 define('APP_NAME', 'Tag');
 define('APP_PATH', './');
-define('APP_DEBUG', 1);
 // 加载框架入口文件
 require( "../../ThinkPHP/ThinkPHP.php");
