03af59a3acaa4faffbded514b44c39a9d945f28e
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-02-22 05:21:42 +0000

    修正路由定义的大写被强制转换为小写的错误 配置参数只支持一级不区分大小写 二级以下配置参数区分大小写调用
    git-svn-id: http://thinkphp.googlecode.com/svn/trunk@2772 539fd3ec-2725-0410-b7ab-251e373a8e33

--- ThinkPHP/Common/common.php
+++ ThinkPHP/Common/common.php
@@ -320,19 +320,19 @@ function L($name=null, $value=null) {
 function C($name=null, $value=null) {
     static $_config = array();
     // 无参数时获取所有
-    if (empty($name))
-        return $_config;
+    if (empty($name))   return $_config;
     // 优先执行设置获取或赋值
     if (is_string($name)) {
-        $name = strtolower($name);
         if (!strpos($name, '.')) {
+            $name = strtolower($name);
             if (is_null($value))
                 return isset($_config[$name]) ? $_config[$name] : null;
-            $_config[$name] = is_array($value)?array_change_key_case($value):$value;
+            $_config[$name] = $value;
             return;
         }
         // 二维数组设置和获取支持
         $name = explode('.', $name);
+        $name[0]   =  strtolower($name[0]);
         if (is_null($value))
             return isset($_config[$name[0]][$name[1]]) ? $_config[$name[0]][$name[1]] : null;
         $_config[$name[0]][$name[1]] = $value;
@@ -340,11 +340,6 @@ function C($name=null, $value=null) {
     }
     // 批量设置
     if (is_array($name)){
-        foreach ($name as $key=>$val){
-            if(is_array($val)) {
-                $name[$key]  =  array_change_key_case($val);
-            }
-        }
         return $_config = array_merge($_config, array_change_key_case($name));
     }
     return null; // 避免非法参数
