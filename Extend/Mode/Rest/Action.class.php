643003b9a46754de42abacc0aa457984c0e827ff
huangdijia <huangdijia@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-03-21 01:05:36 +0000

    修正sendHttpStatus方法参数
    git-svn-id: http://thinkphp.googlecode.com/svn/trunk@2832 539fd3ec-2725-0410-b7ab-251e373a8e33

--- Extend/Mode/Rest/Action.class.php
+++ Extend/Mode/Rest/Action.class.php
@@ -266,7 +266,7 @@ abstract class Action {
     }
 
     // 发送Http状态信息
-    protected function sendHttpStatus($status) {
+    protected function sendHttpStatus($code) {
         static $_status = array(
             // Informational 1xx
             100 => 'Continue',
