fe4725b28d15245f8308586342db269eeb7eaa12
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-06-12 09:17:07 +0000

    改进致命错误时候halt尚未定义的情况
    git-svn-id: http://thinkphp.googlecode.com/svn/trunk@2992 539fd3ec-2725-0410-b7ab-251e373a8e33

--- ThinkPHP/Lib/Core/Think.class.php
+++ ThinkPHP/Lib/Core/Think.class.php
@@ -268,7 +268,7 @@ class Think {
             ob_clean();
             $errorStr = "$errstr ".basename($errfile)." 第 $errline 行.";
             if(C('LOG_RECORD')) Log::write("[$errno] ".$errorStr,Log::ERR);
-            halt($errorStr);
+            function_exists('halt')?halt($errorStr):exit($errorStr);
             break;
           case E_STRICT:
           case E_USER_WARNING:
